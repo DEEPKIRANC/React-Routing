8ae4b9b505a83ce010435191dbb3f9a42ef21cc1
Deep Kiran <cdeepkiran14@gmail.com>
2021-03-14 23:30:38 +0530

    React App Created
    Created a react app and added few components.

---
+++ routing/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from "./nav";
+import About from "./About";
+import Shop from "./Shop";
+function App() {
+  return (
+    <div>
+      
+      <Nav />
+      <About />
+      <Shop />
+    </div>
+  );
+}
+
+export default App;
---
+++ routing/src/nav.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./App.css"
+function Nav() {
+  return (
+    <div class="navbar">
+      <h3>Logo</h3>
+      <ul class="nav-links">
+          <li>About</li>
+          <li>Shop</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
