08a7f10b1d81d66a6ba9e128da2d577c566cb485
Deep Kiran <cdeepkiran14@gmail.com>
2021-03-14 23:39:31 +0530

    Updated App.js - Router module added

--- routing/src/App.js
+++ routing/src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
+import "./App.css";
 import Nav from "./nav";
 import About from "./About";
 import Shop from "./Shop";
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 function App() {
   return (
-    <div>
+    <Router> 
+      <div class="App">
       
-      <Nav />
-      <About />
-      <Shop />
-    </div>
+        
+        <Nav />
+        
+        <Route path="/about" component={About} />
+          
+        
+        <Route path="/shop" component={Shop} />    
+      
+      </div>
+    </Router> 
   );
 }
 
